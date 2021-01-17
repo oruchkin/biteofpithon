b3a559264fd050b7922c8b36f3d6d06ae70af5fe
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-01-17 18:28:25 +0300

    aiport model added migrate complete

--- L4/airline/flights/models.py
+++ L4/airline/flights/models.py
@@ -10,11 +10,9 @@ class Airport(models.Model):
         return f"{self.city} ({self.code})"
 
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"{self.id}: {self.origin} to {self.destination} in {self.duration} min"
-
-        
+        return f"{self.id}: {self.origin} to {self.destination}"
