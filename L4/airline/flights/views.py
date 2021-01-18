1d97b407ab5fb9b22775b933191aae8581b1facc
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-01-18 14:43:51 +0300

    mistake in flights id

--- L4/airline/flights/views.py
+++ L4/airline/flights/views.py
@@ -7,3 +7,13 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flights.html", {
+        "flight" : flight
+    })
+
+
+
