247e6977e957eca296a9e802105de0c6663de933
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-02-01 12:20:16 +0300

    finished

--- howdydj/howdy/articles/urls.py
+++ howdydj/howdy/articles/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('test', views.test, name="test"),
     path('<int:article_id>/', views.detail, name="detail"),
+    path('<int:article_id>/leave_comment/', views.leave_comment, name="leave_comment"),
 ]
