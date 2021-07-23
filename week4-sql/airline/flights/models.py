c8eb870628686d6b861cc43cd280d4e2f0876dc6
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-07-23 13:39:31 +0300

    airplane

---
+++ week4-sql/airline/flights/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return (f"{self.id}: {self.origin} to {self.destination}")
+
+
