4fbd07c1973904dde623781e153e2ef960f740e0
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-07-25 09:58:10 +0300

    skillbox django

---
+++ skillbox/django/02/my_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\oleg\desktop\github\biteofpithon\skillbox\django\02\my_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
