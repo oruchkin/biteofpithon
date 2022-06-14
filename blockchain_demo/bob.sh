99cbd7bccbacf3b28d8284f144fcb3afb2c1f167
Oleg <oruchkin@bk.ru>
2022-06-14 21:35:09 +0300

    block_chain_udemy

---
+++ blockchain_demo/bob.sh
@@ -0,0 +1,6 @@
+activate () {
+     . /Users/oleg/Desktop/blockchain_demo/venv/bin/activate
+}
+activate
+cd blockchain_client
+python blockchain_client.py -p 8082
---
+++ blockchain_demo/node3.sh
@@ -0,0 +1,6 @@
+activate () {
+     . /Users/oleg/Desktop/blockchain_demo/venv/bin/activate
+}
+activate
+cd blockchain
+python blockchain.py -p 5003
