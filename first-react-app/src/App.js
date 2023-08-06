e40166611fda471f1fbf85cc84b28ecfc7647533
Ручкин Олег Аркадьевич <Oleg.Ruchkin@rosbank.ru>
2023-08-06 09:32:29 +0300

    react 1

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,24 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const firstName = 'Oleg';
+    const lastName = "Ruchkin";
+    const age = 31;
+    const job = "Developer";
+    const inputPlaceHolder = 'Enter Your Details const';
+
+    const detailsInputBox = <input placeholder={ inputPlaceHolder } autoComplete/>;
+
+    const mArr = [1, 2, 3, 4];
+
+    const mObj = {
+        name: 'Toto',
+        age: 55,
+    }
+
+    const getFullName = () => {
+        return `${ firstName } ${ lastName }`;
+    };
+
+    return (
+        <div className="App">
+            {/*<h3>Full Name: { `${ firstName } ${ lastName }` }</h3>*/ }
+            <h3>Full Name: { getFullName() }</h3>
+            <p>Age: { mObj.age } </p>
+            <p>Job: { job }</p>
+            {/*<input placeholder={ inputPlaceHolder } type="text"/>*/ }
+            { detailsInputBox }
+            <br/>
+            { mArr[0] }
+            {mObj.name}
+        </div>
+    );
 }
 
 export default App;
