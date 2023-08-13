12385fbf82cd1d0e67ac16a7bbb85aa8400b1ebf
Ручкин Олег Аркадьевич <Oleg.Ruchkin@rosbank.ru>
2023-08-13 20:13:22 +0300

    modules css

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -9,25 +9,25 @@ function App() {
         description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
     };
 
-    // const blogArr = [
-    //     {
-    //         id: 1,
-    //         title: "Blog Title 1",
-    //         description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "Blog Title 2",
-    //         description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "Blog Title 3",
-    //         description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-    //     },
-    // ];
+    const blogArr = [
+        {
+            id: 1,
+            title: "Blog Title 1",
+            description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+        },
+        {
+            id: 2,
+            title: "Blog Title 2",
+            description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+        },
+        {
+            id: 3,
+            title: "Blog Title 3",
+            description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+        },
+    ];
 
-    const blogArr = null;
+    // const blogArr = null;
 
     const blogCards = isArrayEmpty(blogArr) ? [] : blogArr.map((item, pos) => {
         return (
--- first-react-app/src/BlogCard.js
+++ first-react-app/src/BlogCard.js
@@ -1,11 +1,13 @@
-
 import { dumpLogs } from "./Utils";
+// import './BlogCard.css';
+import classes from './BlogCardM.module.css'
 
 const BlogCard = (props) => {
     // console.log(props);
-    dumpLogs(props)
+    dumpLogs(props);
     return (
-        <div className="BlogCard">
+        // <div className="BlogCard">
+        <div className={ classes.NewBlogCard }>
             <h3>{ props.title }</h3>
             <p>{ props.description }</p>
         </div>
